8694272984e3c472d16b526e0f584f48408e1498
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2022-09-07 11:26:54 +0200

    Create brands schema

--- backend_sanity/schemas/schema.js
+++ backend_sanity/schemas/schema.js
@@ -5,6 +5,7 @@ import createSchema from 'part:@sanity/base/schema-creator';
 import schemaTypes from 'all:part:@sanity/base/schema-type';
 import works from './works';
 import testimonials from './testimonials';
+import brands from './brands';
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -15,6 +16,7 @@ export default createSchema({
   types: schemaTypes.concat([
     /* Your types here! */
     works,
-    testimonials
+    testimonials,
+    brands
   ])
 });
