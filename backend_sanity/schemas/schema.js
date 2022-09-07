c46a2f357532e6a35815277b1e20ec059d5748a1
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2022-09-07 11:55:44 +0200

    Create skills schema

--- backend_sanity/schemas/schema.js
+++ backend_sanity/schemas/schema.js
@@ -7,6 +7,7 @@ import works from './works';
 import testimonials from './testimonials';
 import brands from './brands';
 import abouts from './abouts';
+import skills from './skills';
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -19,6 +20,7 @@ export default createSchema({
     works,
     testimonials,
     brands,
-    abouts
+    abouts,
+    skills
   ])
 });
