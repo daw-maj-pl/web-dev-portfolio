5d0a53f1f4df90ff45a85a02e5a23d28699abf03
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2022-09-16 17:11:20 +0200

    Re-export redux, sass, typescript, profile and circle images

--- frontend_react/src/constants/images.js
+++ frontend_react/src/constants/images.js
@@ -1,3 +1,9 @@
+import redux from '../assets/redux.png';
+import sass from '../assets/sass.png';
+import typescript from '../assets/typescript.png';
+
+import profile from '../assets/profile.png';
+import circle from '../assets/circle.svg';
 import logo from '../assets/logo.png';
 
-export default { logo };
+export default { redux, sass, typescript, profile, circle, logo };
