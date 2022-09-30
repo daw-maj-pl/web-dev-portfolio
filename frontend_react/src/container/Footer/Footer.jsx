194d7a2a0a0542c2143a6965e327bbfde4ff78d2
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2022-09-30 11:15:16 +0200

    Create Footer component

---
+++ frontend_react/src/container/Footer/Footer.jsx
@@ -0,0 +1,121 @@
+import { useState, useEffect } from 'react';
+import { images } from '../../constants';
+import { AppWrap, MotionWrap } from '../../wrapper';
+import { client } from '../../client';
+import './Footer.scss';
+
+const Footer = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: ''
+  });
+  const [isFormSubmitted, setIsFormSubmitted] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [myContactData, setMyContactData] = useState([]);
+
+  const { name, email, message } = formData;
+
+  useEffect(() => {
+    const query = '*[_type == "myContactData"]';
+
+    client.fetch(query).then(data => {
+      setMyContactData(data);
+    });
+  }, []);
+
+  const handleChangeInput = e => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = () => {
+    setLoading(true);
+
+    const contact = {
+      _type: 'contact',
+      name: name,
+      email: email,
+      message: message
+    };
+
+    client.create(contact).then(() => {
+      setLoading(false);
+      setIsFormSubmitted(true);
+    });
+  };
+
+  return (
+    <>
+      <h2 className="head-text">Get In Touch</h2>
+      <div className="app__footer-cards">
+        <div className="app__footer-card">
+          <img src={images.email} alt="email" />
+          <a
+            href={`mailto:${
+              myContactData.length ? myContactData[0].email : ''
+            }`}
+            className="p-text"
+          >
+            {myContactData.length ? myContactData[0].email : ''}
+          </a>
+        </div>
+        <div className="app__footer-card">
+          <img src={images.mobile} alt="mobile" />
+          <a
+            href={`tel:${myContactData.length ? myContactData[0].mobile : ''}`}
+            className="p-text"
+          >
+            {myContactData.length ? myContactData[0].mobile : ''}
+          </a>
+        </div>
+      </div>
+      {!isFormSubmitted ? (
+        <div className="app__footer-form app__flex">
+          <div className="app__flex">
+            <input
+              className="p-text"
+              type="text"
+              placeholder="Your Name"
+              name="name"
+              value={name}
+              onChange={handleChangeInput}
+            />
+          </div>
+          <div className="app__flex">
+            <input
+              className="p-text"
+              type="email"
+              placeholder="Your Email"
+              name="email"
+              value={email}
+              onChange={handleChangeInput}
+            />
+          </div>
+          <div>
+            <textarea
+              className="p-text"
+              placeholder="Your Message"
+              value={message}
+              name="message"
+              onChange={handleChangeInput}
+            />
+          </div>
+          <button type="button" className="p-text" onClick={handleSubmit}>
+            {loading ? 'Sending...' : 'Send Message'}
+          </button>
+        </div>
+      ) : (
+        <div>
+          <h3 className="head-text">Thank you for getting in touch!</h3>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default AppWrap(
+  MotionWrap(Footer, 'app__footer'),
+  'contact',
+  'app__whitebg'
+);
